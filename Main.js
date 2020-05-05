f31c4a6611656b31e227eaeb905cf44424bf76e1
totinos-heathen <totinos.heathen@gmail.com>
2020-05-05 10:04:13 -0600

    Updates for version info

--- Main.js
+++ Main.js
@@ -4,8 +4,10 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./realconfig.json");
+const package = require("./package.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
+client.package = package;
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
     message.channel.send("Returns spell descriptions for Starfinder.\n" +
-        "Made by Ada, message me at totinos.heathen@gmail.com\nVersion 1.1, created May 5th, 2020").catch(console.error);
+        "Made by Ada, message me at totinos.heathen@gmail.com\nVersion " + client.package.version).catch(console.error);
 }
